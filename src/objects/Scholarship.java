05f0bc03b46e3a9e456d1d14c4ffaa6e679e9f39
Steve Khanna <stvkhanna@gmail.com>
2019-03-03 19:27:12 -0700

    Objects have been commented

--- src/objects/Scholarship.java
+++ src/objects/Scholarship.java
@@ -1,11 +1,17 @@
 package objects;
+
 /**
  * Scholarship class contains all attributes for scholarship
+ * 
  * @author Steve Khanna
  *
  *
  */
 public class Scholarship {
+
+	/**
+	 * Instance Variables
+	 */
 	private String name;
 	private int scholarshipId;
 	private double gpaRequirement;
@@ -16,91 +22,132 @@ public class Scholarship {
 	private int numAllowed;
 	private String description;
 
-
+	/**
+	 * Default scholarship construtor
+	 */
 	public Scholarship() {
-		this.name ="Sch1";
+		this.name = "Sch1";
 		this.scholarshipId = 1;
-		this.gpaRequirement=3.00;
-		this.faculty="Science";
-		this.yearOfStudy= 3;
-		this.typeOfStudy ="Bachelor";
+		this.gpaRequirement = 3.00;
+		this.faculty = "Science";
+		this.yearOfStudy = 3;
+		this.typeOfStudy = "Bachelor";
 		this.Department = "CPSC";
 		this.numAllowed = 100;
-		this.description="This is a generic scholarship";
+		this.description = "This is a generic scholarship";
 
 	}
-
-  public Scholarship(String name, String sId, String gReq, String faculty, String YOS, String TOS, String Dept, String nums, String desc) {
-		this.name =name;
+	
+	/**
+	 * Overloaded scholarship controller
+	 * @param name 
+	 * @param sId
+	 * @param gReq
+	 * @param faculty
+	 * @param YOS
+	 * @param TOS
+	 * @param Dept
+	 * @param nums
+	 * @param desc
+	 * This is used when a new scholarship is created
+	 */
+	public Scholarship(String name, String sId, String gReq, String faculty, String YOS, String TOS, String Dept,
+			String nums, String desc) {
+		this.name = name;
 		this.scholarshipId = Integer.parseInt(sId);
-		this.gpaRequirement=Double.parseDouble(gReq);
-		this.faculty=faculty;
-		this.yearOfStudy= Integer.parseInt(YOS);
-		this.typeOfStudy =TOS;
+		this.gpaRequirement = Double.parseDouble(gReq);
+		this.faculty = faculty;
+		this.yearOfStudy = Integer.parseInt(YOS);
+		this.typeOfStudy = TOS;
 		this.Department = Dept;
 		this.numAllowed = Integer.parseInt(nums);
-		this.description=desc;
+		this.description = desc;
 
 	}
-  public String returnFull(){
-    return (name+":"+scholarshipId+":"+gpaRequirement+":"+faculty+":"+yearOfStudy+":"+typeOfStudy+":"+Department+":"+numAllowed+":"+description);
-  }
+	
+	/**
+	 * returnFull method
+	 * @returns String of all scholarship attributes with colons separating them
+	 */
 
-
+	public String returnFull() {
+		return (name + ":" + scholarshipId + ":" + gpaRequirement + ":" + faculty + ":" + yearOfStudy + ":"
+				+ typeOfStudy + ":" + Department + ":" + numAllowed + ":" + description);
+	}
+	/**
+	 * 
+	 * The rest of the code contains getters and setters for each attribute
+	 */
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getScholarshipId() {
 		return scholarshipId;
 	}
+
 	public void setScholarshipId(int scholarshipId) {
 		this.scholarshipId = scholarshipId;
 	}
+
 	public double getGpaRequirement() {
 		return gpaRequirement;
 	}
+
 	public void setGpaRequirement(double gpaRequirement) {
 		this.gpaRequirement = gpaRequirement;
 	}
+
 	public String getFaculty() {
 		return faculty;
 	}
+
 	public void setFaculty(String faculty) {
 		this.faculty = faculty;
 	}
+
 	public int getYearOfStudy() {
 		return yearOfStudy;
 	}
+
 	public void setYearOfStudy(int yearOfStudy) {
 		this.yearOfStudy = yearOfStudy;
 	}
+
 	public String getTypeOfStudy() {
 		return typeOfStudy;
 	}
+
 	public void setTypeOfStudy(String typeOfStudy) {
 		this.typeOfStudy = typeOfStudy;
 	}
+
 	public String getDepartment() {
 		return Department;
 	}
+
 	public void setDepartment(String department) {
 		Department = department;
 	}
+
 	public int getNumAllowed() {
 		return numAllowed;
 	}
+
 	public void setNumAllowed(int numAllowed) {
 		this.numAllowed = numAllowed;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
 
 }
