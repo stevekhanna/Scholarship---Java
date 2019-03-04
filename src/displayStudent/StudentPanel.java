5e137f83deeb57b7fc22c61ceee1c6322e9e4b08
Steve Khanna <stvkhanna@gmail.com>
2019-03-03 19:47:40 -0700

    Comments to student panel

--- src/displayStudent/StudentPanel.java
+++ src/displayStudent/StudentPanel.java
@@ -8,16 +8,33 @@ import javax.swing.JLabel;
 
 import myJStuff.*;
 import objects.Scholarship;
+
+/**
+ * Student Panel class displays a list of scholarships and 
+ * ther person who is currently logged in 
+ * @author Pierce de Jong, Steve Khanna
+ *
+ */
 public class StudentPanel extends MyPanel{
 	
+	/**
+	 * Instance Variables
+	 */
 	private ActionListener globalListener;
-	
+	private int y =0;
 	JLabel lblLoggedin;
 	
 	JLabel lblStudent;
 	
 	JButton btnBack;
 	
+	/**
+	 * Constructor with arguement of ActionListener packageListener
+	 * This constructor will initialize the packageListener as well as call upon
+	 * the other methods of the class to be displayed
+	 * @param packageListener
+	 * @param globalListener
+	 */
 	public StudentPanel(ActionListener packageListener, ActionListener globalListener) {
 		this.packageListener = packageListener;
 		this.globalListener = globalListener;
@@ -26,13 +43,19 @@ public class StudentPanel extends MyPanel{
 		displaySouth();
 	}
 	
+	
+	/**
+	 * Method to display button at the north end of the screen
+	 */
 	private void displayNorth(){
 		lblLoggedin = new MyLabel("Logged in", textColor, Size.defaultLblTitleFontSize);
 		north.add(lblLoggedin, "cell 0 0,center");
 
 		lblStudent = new MyLabel("Student", textColor, Size.defaultLblFontSize);
 		north.add(lblStudent, "cell 0 1,center");}
-	
+	/**
+	 * Method to display button button at the south of the screen
+	 */
 	private void displaySouth() {
 		btnBack = new MyButton("Log out", btnTxtColor, btnBackgroundColor, Size.defaultBtnFontSize);
 		south.add(btnBack, "cell 0 0");
@@ -41,12 +64,18 @@ public class StudentPanel extends MyPanel{
 	}
 	
 	
-	
+	/**
+	 * 
+	 * @param name sets the label name to this name
+	 */
 	public void setLblLoggedin(String name) {
 		lblLoggedin.setText(name);
 	}
-
-	private int y =0;
+	
+	/**
+	 * Method that displays the scholarship
+	 * @param scholarship -to display
+	 */
 	public void displayScholarship(Scholarship scholarship) {
 		String name = scholarship.getName();
 		String gPA = Double.toString(scholarship.getGpaRequirement());
